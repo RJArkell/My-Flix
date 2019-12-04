d864a291e73fd5ea1db09a3ef89d52765e7dff73
RJArkell <51424552+RJArkell@users.noreply.github.com>
2019-12-03 23:55:24 -0500

    Updated User gets and director list

--- index.js
+++ index.js
@@ -6,54 +6,107 @@ const express = require('express'),
 
 //List of movies//
 let Movies = [{
-    title : 'Alien',
-    director : 'Ridley Scott',
-    year: '1979'
+    title: 'Alien',
+    director: 'Ridley Scott',
+    year: '1979',
+    genre: 'Science-Fiction', 'Horror'
   },
   {
-    title : 'Predator',
-    director : 'John McTiernan',
-    year: '1987'
+    title: 'Predator',
+    director: 'John McTiernan',
+    year: '1987',
+    genre: 'Science-Fiction', 'Action'
   },
   {
-    title : 'Se7en',
-    director : 'David Fincher',
-    year: '1995'
+    title: 'Se7en',
+    director: 'David Fincher',
+    year: '1995',
+    genre: 'Thriller', 'Neo-Noir'
   },
   {
-    title : 'Coherence',
-    director : 'James Ward Byrkit',
-    year: '2013'
+    title: 'Coherence',
+    director: 'James Ward Byrkit',
+    year: '2013',
+    genre: 'Science-Fiction', 'Thriller'
   },
   {
-    title : 'Mad Max: Fury Road',
-    director : 'George Miller',
-    year: '2015'
+    title: 'Mad Max: Fury Road',
+    director: 'George Miller',
+    year: '2015',
+    genre: 'Post-Apocalyptic', 'Action'
   },
   {
-    title : '28 Days Later',
-    director : 'Danny Boyle',
-    year: '2002'
+    title: '28 Days Later',
+    director: 'Danny Boyle',
+    year: '2002',
+    genre: 'Post-Apocalyptic', 'Horror'
   },
   {
-    title : 'The Thing',
-    director : 'John Carpenter',
-    year: '1982'
+    title: 'The Thing',
+    director: 'John Carpenter',
+    year: '1982',
+    genre: 'Science-Fiction', 'Horror'
   },
   {
-    title : 'The Raid',
-    director : 'Gareth Evans',
-    year: '2011'
+    title: 'The Raid',
+    director: 'Gareth Evans',
+    year: '2011',
+    genre: 'Action', 'Thriller'
   },
   {
-    title : 'Drive',
-    director : 'Nicolas Winding Refn',
-    year: '2011'
+    title: 'Drive',
+    director: 'Nicolas Winding Refn',
+    year: '2011',
+    genre: 'Thriller', 'Neo-Noir'
   },
   {
-    title : 'Bone Tomahawk',
-    director : 'S. Craig Zahler',
-    year: '2015'
+    title: 'Bone Tomahawk',
+    director: 'S. Craig Zahler',
+    year: '2015',
+    genre: 'Horror', 'Western'
+  }
+]
+
+//List of directors//
+let Directors = [{
+    name: 'Ridley Scott',
+    born: '1937'
+  },
+  {
+    name: 'John McTiernan',
+    born: '1951'
+  },
+  {
+    name: 'David Fincher',
+    born: '1962'
+  },
+  {
+    name: 'James Ward Byrkit',
+    born: '?'
+  },
+  {
+    name: 'George Miller',
+    born: '1945'
+  },
+  {
+    name: 'Danny Boyle',
+    born: '1956'
+  },
+  {
+    name: 'John Carpenter',
+    born: '1948'
+  },
+  {
+    name: 'Gareth Evans',
+    born: '1980'
+  },
+  {
+    name: 'Nicolas Winding Refn',
+    born: '1970'
+  },
+  {
+    name: 'S. Craig Zahler',
+    born: '1973'
   }
 ]
 
@@ -67,6 +120,10 @@ app.use(morgan('common'));
 //Get list of movies//
 app.get('/movies', (req, res) => {
   res.json(Movies)
+});
+//Get list of directors//
+app.get('/directors', (req, res) => {
+  res.json(Directors)
 });
 //Get movie info//
 app.get('/movies/:title', (req, res) => {
@@ -82,6 +139,53 @@ app.get('/genres/:name', (req, res) => {
 app.get('/directors/:name', (req, res) => {
   res.json(Directors.find((director) =>
   {return director.name === req.params.name}));
+});
+
+//Create user//
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    Users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+//Update user info//
+app.put('/users/:username', (req, res) => {
+    res.status(201).send('User information updated');
+});
+//Delete user//
+app.delete('/users/:id', (req, res) => {
+  let user = Users.find((user) => { return user.id === req.params.id});
+  if (user) {
+    Users = Users.filter(function(obj) { return obj.id !== req.params.id});
+    res.status(201).send('User ' + user.name + '(ID:'+ req.params.id + ') has been deleted.')
+  }
+});
+
+//Add favorite movie//
+app.post('/users/:username/favorites', (req, res) => {
+  let newFavorite = req.body;
+  if (!newFavorite.title) {
+    const message = 'Missing movie title in request body';
+    res.status(400).send(message);
+  } else {
+    newFavorite.id = uuid.v4();
+    Favorites.push(newFavorite);
+    res.status(201).send(newFavorite);
+  }
+});
+//Remove favorite movie//
+app.delete('/users/:username/favorites/:title', (req, res) => {
+  let favorite = Favorites.find((favorite) =>
+    {return favorite.title === req.params.title});
+  if (favorite) {
+    Favorites.filter(function(obj) {return obj.title !== req.params.title});
+    res.status(201).send(req.params.title + ' was removed from favorites.');
+  }
 });
 
 //Error catching//
