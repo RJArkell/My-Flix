d6fcce74632e717818147b5d785ab7b0639c6de8
RJArkell <rjarkell1@gmail.com>
2019-12-31 19:53:45 -0500

    Remove JWT authentication from /movies

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 });
 
 //Get list of movies//
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies'), (req, res) => {
   Movies.find()
     .then((movies) => { res.status(201).json(movies) })
     .catch((err) => {
