6960822a9b42062e4657e967e1bbcc5f6c2f036e
RJArkell <rjarkell1@gmail.com>
2020-01-07 21:59:51 -0500

    Fixed models.js

--- models.js
+++ models.js
@@ -4,15 +4,15 @@ const bcrypt = require('bcrypt');
 //Schema for genres//
 var genreSchema = mongoose.Schema({
   _id: String,
-  Name: {type: String, required: true},
-  Description: {type: String, required: true},
+  Name: { type: String, required: true },
+  Description: { type: String, required: true },
 });
 
 //Schema for directors//
 var directorSchema = mongoose.Schema({
   _id: String,
-  Name: {type: String, required: true},
-  Biography: {type: String, required: true},
+  Name: { type: String, required: true },
+  Biography: { type: String, required: true },
   Born: String,
   Died: String
 });
@@ -20,17 +20,17 @@ var directorSchema = mongoose.Schema({
 //Schema for movies//
 var movieSchema = mongoose.Schema({
   _id: String,
-  Title: {type: String, required: true},
-  Description: {type: String, required: true},
-  Genre: [{type: mongoose.Schema.Types.ObjectId, ref: 'Genre'}],
-  Director: [{type: mongoose.Schema.Types.ObjectId, ref: 'Director'}],
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: { type: mongoose.Schema.Types.ObjectId, ref: 'Genre' },
+  Director: { type: mongoose.Schema.Types.ObjectId, ref: 'Director' },
   Actors: [String],
   ImagePath: String,
   Featured: Boolean
 });
 
 //Autopopulate the genre and director fields for movies//
-var autoPopulateMovies = function(next) {
+var autoPopulateMovies = function (next) {
   this.populate('Genre');
   this.populate('Director');
   next();
@@ -42,19 +42,21 @@ movieSchema.
 
 //Schema for users//
 var userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
   Birthday: Date,
-  FavoriteMovies: [{type: mongoose.Schema.Types.String, ref: 'Movie'}]
+  FavoriteMovies: [{ type: mongoose.Schema.Types.String, ref: 'Movie' }]
 });
 
 //Bcrypt password encryption//
-userSchema.statics.hashPassword = function(password) {
-  return bcrypt.hashSync(password, 10); };
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hashSync(password, 10);
+};
 
-userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.Password); };
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 //Models for all schemas//
 var Genre = mongoose.model('Genre', genreSchema);
